9fb4a72314e779c628fe4506da7e953721162847
JacobRyanHanson <jacobryanhanson@gmail.com>
2021-11-25 10:13:11 -0600

    Add post routes

--- models/User.js
+++ models/User.js
@@ -1,43 +1,58 @@
 const { Model, DataTypes } = require('sequelize');
 const sequelize = require('../config/connection');
+const bcrypt = require('bcrypt');
 
-class User extends Model {}
+class User extends Model {
+    checkPassword(loginPw) {
+        return bcrypt.compareSync(loginPw, this.password);
+    }
+}
 
 User.init(
     {
-        id: 
+        id:
         {
             type: DataTypes.INTEGER,
             allowNull: false,
             primaryKey: true,
             autoIncrement: true
         },
-        username: 
+        username:
         {
             type: DataTypes.STRING,
             allowNull: false
         },
-        email: 
+        email:
         {
             type: DataTypes.STRING,
             allowNull: false,
             unique: true,
-            validate: 
+            validate:
             {
                 isEmail: true
             }
         },
-        password: 
+        password:
         {
             type: DataTypes.STRING,
             allowNull: false,
-            validate: 
+            validate:
             {
                 len: [4]
             }
         }
     },
     {
+        hooks: {
+            async beforeCreate(newUserData) {
+                newUserData.password = await bcrypt.hash(newUserData.password, 10);
+                return newUserData;
+            },
+            async beforeUpdate(updatedUserData) {
+                updatedUserData.password = await bcrypt.hash(updatedUserData.password, 10);
+                return updatedUserData;
+            }
+        },
         sequelize,
         timestamps: false,
         freezeTableName: true,
--- routes/api/user-routes.js
+++ routes/api/user-routes.js
@@ -2,7 +2,7 @@ const router = require('express').Router();
 const { User } = require('../../models');
 
 router.get('/', (req, res) => {
-    // Access our User model and run .findAll() method)
+    // Access our User model and run .findAll() method
     User.findAll({
         attributes: { exclude: ['password'] }
     }).then(dbUserData => res.json(dbUserData)).catch(err => {
@@ -41,9 +41,31 @@ router.post('/', (req, res) => {
     });
 });
 
+router.post('/login', (req, res) => {
+    User.findOne({
+        where: {
+            email: req.body.email
+        }
+    }).then(dbUserData => {
+        if (!dbUserData) {
+            res.status(400).json({ message: 'No user with that email address!' });
+            return;
+        }
+        // Verify user
+        const validPassword = dbUserData.checkPassword(req.body.password);
+        if (!validPassword) {
+            res.status(400).json({ message: 'Incorrect password!' });
+            return;
+        }
+
+        res.json({ user: dbUserData, message: 'You are now logged in!' });
+    });
+});
+
 // PUT /api/users/1
 router.put('/:id', (req, res) => {
     User.update(req.body, {
+        individualHooks: true,
         where: {
             id: req.params.id
         }
@@ -65,18 +87,16 @@ router.delete('/:id', (req, res) => {
         where: {
             id: req.params.id
         }
-    })
-        .then(dbUserData => {
-            if (!dbUserData) {
-                res.status(404).json({ message: 'No user found with this id' });
-                return;
-            }
-            res.json(dbUserData);
-        })
-        .catch(err => {
-            console.log(err);
-            res.status(500).json(err);
-        });
+    }).then(dbUserData => {
+        if (!dbUserData) {
+            res.status(404).json({ message: 'No user found with this id' });
+            return;
+        }
+        res.json(dbUserData);
+    }).catch(err => {
+        console.log(err);
+        res.status(500).json(err);
+    });
 });
 
 module.exports = router;
